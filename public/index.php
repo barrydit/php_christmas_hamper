c90b3c716e0a1637906ee83cbd7b5f28048bb301
Barry Dick <barryd.it@gmail.com>
2023-01-25 07:42:29 -0800

    Update index.php
    Added some code from composer to update packages

--- public/index.php
+++ public/index.php
@@ -1,21 +1,100 @@
 <?php
 
 defined('APP_BASE_PATH') // $_SERVER['DOCUMENT_ROOT']
-  or define('APP_BASE_PATH', dirname(__DIR__, 1));
+  or define('APP_BASE_PATH', dirname(__DIR__, 1) . DIRECTORY_SEPARATOR);
+ 
+require(APP_BASE_PATH . 'config/config.php');
+
+// https://stackoverflow.com/questions/38396046/how-to-run-composer-update-on-php-server
+
+define('HOME_DIRECTORY', APP_BASE_PATH . 'composer' );
+define('COMPOSER_INITED', file_exists(APP_BASE_PATH.'/vendor'));
+
+set_time_limit(100);
+ini_set('memory_limit',-1);
+
+if (!getenv('HOME') && !getenv('COMPOSER_HOME')) {
+    putenv("COMPOSER_HOME=".HOME_DIRECTORY);
+}
+
+// change directory to root
+chdir('../');
+
+ini_set('phar.readonly',0);
+if (!file_exists('composer'))
+  (new Phar("composer.phar"))->extractTo("./composer");
+    
+//This requires the phar to have been extracted successfully.
+require_once ('composer/vendor/autoload.php');
+
+//Use the Composer classes
+use Composer\Console\Application;
+use Composer\Command\UpdateCommand;
+use Symfony\Component\Console\Input\ArrayInput;
 
 defined('COMPOSER_AUTOLOAD_PATH')
-  or define("COMPOSER_AUTOLOAD_PATH", APP_BASE_PATH . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR); // basename(dirname(__FILE__)) . DIRECTORY_SEPARATOR . '..' .
+  or define("COMPOSER_AUTOLOAD_PATH", APP_BASE_PATH . 'vendor' . DIRECTORY_SEPARATOR); // basename(dirname(__FILE__)) . DIRECTORY_SEPARATOR . '..' .
 
-require(COMPOSER_AUTOLOAD_PATH . 'autoload.php'); // composer dump -o
+if (!is_dir(COMPOSER_AUTOLOAD_PATH)) {
+  if (!is_file(APP_BASE_PATH . 'composer.phar'))
+    file_put_contents(APP_BASE_PATH . 'composer.phar', file_get_contents('https://getcomposer.org/download/latest-stable/composer.phar'));
+  //chdir();
+  //shell_exec("cd ../ && php -f composer.phar", $output, $worked); // --dry-run --no-interaction --ansi
+  // config --global --auth github-oauth.github.com ghp_1XhQL4hgdghjjyuuyyuTfux51ZDHZz
+
+//Create the commands
+//$args = array('command' => 'self-update');
+$args = array('command' => 'update');
+//$args = array('command' => 'config');
+
+if(!file_exists('vendor')) { 
+    echo "This is first composer run: --no-scripts option is applies\n";
+    $args['--no-scripts'] = true;   
+    //$args['--global'] = NULL;
+    //$args['--editor'] = true;
+    //$args['--auth'] = [ "github-oauth" => [ "github.com" => "ghp_1XhQL4LghjghjghfjhlXqTfux51ZDHZz" ] ] ;
+    //$args['--unset'] = [ "github-oauth" => [ "github.com" => "ghp_1XhQL4LWTl3KtyJmmWlIjfghjfghjfufgh" ] ] ;
+    //$args['github-oauth.github.com'] = 'ghp_1XhQLfhgjgfjhghjghjux51ZDHZz';   
+}
+$input = new ArrayInput($args);
+
+//Create the application and run it with the commands
+$application = new Application();
+$application->setAutoExit(false);
+$application->setCatchExceptions(false);
+try {
+    //Running commdand php.ini allow_url_fopen=1 && proc_open() function available
+    $application->run($input);
+    echo 'Success';
+} catch (\Exception $e) {
+    echo 'Error: '.$e->getMessage()."\n";
+}
+/*
+  die(var_dump(shell_exec("cd ../ && php -f composer.phar")));
+  //die(var_dump(shell_exec('composer config --global --auth github-oauth.github.com ghp_1XhQL4LWTl3Kghjfghjfghjjgfgh')));
+  $empty1=array();
+  $empty2=array();
+  $proc=proc_open('php composer.phar config --global --auth github-oauth.github.com ghp_1XhQLhgjghjghjghjghjHZz',$empty1,$empty2 );
+  $ret = proc_close($proc);
+  
+  die(var_dump(passthru('php composer.phar config --global --auth github-oauth.github.com ghp_1XbnmbnmbnmbnmqTfux51ZDHZz')));
+  exec('php composer.phar update --no-interaction --quiet 2>&1', $output, $worked); // self-update
+*/
+  die(header('Location: http://' . APP_BASE_URL));
+  
+} else 
+  require(COMPOSER_AUTOLOAD_PATH . 'autoload.php'); // composer dump -o
 
 switch($_SERVER['SERVER_NAME']) {
   case stristr($_SERVER['SERVER_NAME'], isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : $_SERVER['SERVER_NAME']):
     if (!isset($_SERVER['HTTPS']) && @$_SESSION['enable_ssl'] == TRUE):
       //exit(header('Location: ' . preg_replace("/^http:/i", "https:", APP_BASE_URL ))); // basename($_SERVER['REQUEST_URI']));
     endif;
-    if ($_SERVER['SERVER_NAME'] == 'localhost')
+    if ($_SERVER['SERVER_NAME'] == 'localhost') {
+      if (!is_file(APP_BASE_PATH . '.env.development'))
+        file_put_contents(APP_BASE_PATH . '.env.development', "DB_UNAME=root\nDB_PWORD=");
       $dotenv = Dotenv\Dotenv::createImmutable(APP_BASE_PATH, '.env.development');
-    else
+    } else
       $dotenv = Dotenv\Dotenv::createImmutable(APP_BASE_PATH, '.env.production');
     $dotenv->safeLoad();
     break;
@@ -28,7 +107,6 @@ switch($_SERVER['SERVER_NAME']) {
 }
 
 $includeFiles = [
-  APP_BASE_PATH . '/config/config.php',
   APP_BASE_PATH . '/config/database.php',
   APP_BASE_PATH . '/config/session.php',
 
